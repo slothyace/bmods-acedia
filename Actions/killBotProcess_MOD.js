da38485886310838cc73d871941b1f9cfde24e4a
Acedia <110048632+slothyace@users.noreply.github.com>
2024-11-21 09:13:53 +0800

    updat src

--- Actions/killBotProcess_MOD.js
+++ Actions/killBotProcess_MOD.js
@@ -4,7 +4,7 @@ module.exports = {
     },
     category: "Control",
     info:{
-        source: "https://github.com/slothyace/bmd-ace/tree/main/Actions",
+        source: "https://github.com/slothyace/bmods-acedia/tree/main/Actions",
         creator: "Acedia",
         donate: "https://ko-fi.com/slothyacedia"
     },
