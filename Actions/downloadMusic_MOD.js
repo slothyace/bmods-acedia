bc32c9f4de44602fcf90634b77f3bbd45a700aa6
Acedia <110048632+slothyace@users.noreply.github.com>
2024-12-28 04:57:56 +0800

    op

--- Actions/downloadMusic_MOD.js
+++ Actions/downloadMusic_MOD.js
@@ -10,8 +10,8 @@ module.exports ={
   aliases: ["Download YouTube Audio", "Download Audio"],
   modules: ["fs", "node:child_process"],
   info: {
-    source: "https://github.com/slothyace/bmods-acedia/tree/main/QOLs",
-    creator: "Acedia QOLs",
+    source: "https://github.com/slothyace/bmods-acedia/tree/main/Actions",
+    creator: "Acedia",
     donate: "https://ko-fi.com/slothyacedia",
   },
   category: "Utilities",
