8638ee7b36276bf7c3f628cf69aec2a26f5c8c99
Acedia <110048632+slothyace@users.noreply.github.com>
2025-06-03 06:17:06 +0800

    map and sort json data

--- WIPs/functionsToSave.js
+++
@@ -1,5 +0,0 @@
-function createConsoleTimestamp (date = new Date()){
-  const pad = (num)=> num.toString().padStart(2, "0")
-  const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-  return `${pad(date.getDate())}-${months[date.getMonth()]}-${String(date.getFullYear()).slice(-2)}@${pad(date.getHours())}:${pad(date.getMinutes())}:${pad(date.getSeconds())}`
-}
