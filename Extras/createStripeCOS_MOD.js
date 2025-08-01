bc5af60a6d6cbd9f0e707c93e8fb5e981e52f51b
Acedia <110048632+slothyace@users.noreply.github.com>
2025-08-02 00:19:49 +0800

    woops, missed a void

--- Extras/createStripeCOS_MOD.js
+++ Extras/createStripeCOS_MOD.js
@@ -43,7 +43,7 @@ module.exports = {
           currencies[currency] = {name: `${currency.toUpperCase()}`, field:false}
         })
         return currencies
-      })
+      })()
     },
     "-",
     {
