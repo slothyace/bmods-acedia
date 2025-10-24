55e717e20ea534a360121967b85d001198f2690e
Acedia <110048632+slothyace@users.noreply.github.com>
2025-10-24 20:49:21 +0800

    remove qols folder

--- merge.bat
+++ merge.bat
@@ -1,10 +1,8 @@
 xcopy /y "Fixes" "./Actions"
-xcopy /y "QOLs" "./Actions"
 xcopy /y "Tweaks" "./Actions"
 echo Merging Done.
 timeout /t 5
 rmdir /s /q "Fixes"
-rmdir /s /q "QOLs"
 rmdir /s /q "Tweaks"
 echo Cleanup Done.
 timeout /t 5
--- mergeExtras.bat
+++ mergeExtras.bat
@@ -1,11 +1,9 @@
 xcopy /y "Fixes" "./Actions"
-xcopy /y "QOLs" "./Actions"
 xcopy /y "Tweaks" "./Actions"
 xcopy /y "Extras" "./Actions"
 echo Merging Done.
 timeout /t 5
 rmdir /s /q "Fixes"
-rmdir /s /q "QOLs"
 rmdir /s /q "Tweaks"
 rmdir /s /q "Extras"
 echo Cleanup Done.
