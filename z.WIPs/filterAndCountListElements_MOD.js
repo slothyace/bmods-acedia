e0b6b775b63094f7a1a7746f9da8be0333d2f917
Acedia <110048632+slothyace@users.noreply.github.com>
2025-11-05 18:29:43 +0800

    IM USING THE LAPTOP INSTEAD

---
+++ z.WIPs/filterAndCountListElements_MOD.js
@@ -0,0 +1,77 @@
+modVersion = "v1.0.0"
+module.exports = {
+  data: {
+    name: "Filter And Count List Elements"
+  },
+  aliases: [],
+  modules: [],
+  category: "Lists",
+  info: {
+    source: "https://github.com/slothyace/bmods-acedia/tree/main/Actions",
+    creator: "Acedia",
+    donate: "https://ko-fi.com/slothyacedia",
+  },
+  UI: [
+    {
+      element: "variable",
+      storeAs: "inputList",
+      name: "List",
+    },
+    "_",
+    {
+      element: "menu",
+      storeAs: "listElements",
+      name: "Look For Elements",
+      types: {
+        elements: "elements"
+      },
+      max: 1000,
+      UItypes: {
+        elements: {
+          data: {},
+          name: "Element",
+          preview: "`${option.data.elementValue}, Case Sensitive: ${option.data.caseSens}`",
+          UI: [
+            {
+              element: "input",
+              storeAs: "elementValue",
+              name: "Element Value (Filter For)",
+              placeholder: "A"
+            },
+            {
+              element: "toggle",
+              storeAs: "caseSens",
+              name: "Case Sensitive?",
+            },
+            "-",
+            {
+              element: "store",
+              storeAs: "store",
+              name: "Store Number Of Occurences As"
+            }
+          ]
+        }
+      }
+    },
+    "-",
+    {
+      element: "text",
+      text: modVersion
+    }
+  ],
+
+  subtitle: (values, constants, thisAction) =>{ // To use thisAction, constants must also be present
+    return `Look And Count For ${values.elements.length} Elements`
+  },
+
+  compatibility: ["Any"],
+
+  async run(values, message, client, bridge){ // This is the exact order of things required, other orders will brick
+    for (const moduleName of this.modules){
+      await client.getMods().require(moduleName)
+    }
+    
+    let inputList = bridge.get(values.inputList)
+    
+  }
+}
